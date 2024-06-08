4c1053519197ae5f64a5b161a03c1d9a8fb21a23
innTall <avg999.trade@gmail.com>
2024-06-08 11:24:00 +0200

    feat: add option to get msg from server

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/watch", ticker);
 app.use("/options", options);
+app.get("/", (req, res) => {
+  res.send("App on Render server");
+});
 app.listen(port, (err) => {
   if (err) console.log(err);
   console.log(`Server running on port: ${port}`);
